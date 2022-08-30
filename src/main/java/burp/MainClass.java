3a0eb0f82a498b8a68ee3e17ac560d6d9edda614
Lam Scun <facebook89911192@gmail.com>
2022-08-31 01:09:53 +0700

    Add files via upload

---
+++ src/main/java/burp/MainClass.java
@@ -0,0 +1,26 @@
+package burp;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MainClass {
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		
+
+//	    reader.close();
+		Random r = new Random();
+//		System.out.println(list_random_biids[r.nextInt(list_random_biids.length)]);
+	  }
+}
